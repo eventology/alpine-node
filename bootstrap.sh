6c5fb42616f1341900b0e91845c5c36773a114b8
Chance Hudson <jchancehud@gmail.com>
2017-05-25 14:48:34 -0500

    Updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-aws s3 cp ${S3_CONFIG_PATH} ./ --recursive
+if [ -n "$S3_CONFIG_PATH" ]; then
+  echo "No S3 config path supplied, skipping copy phase"
+else
+  echo "Downloading S3 config from $S3_CONFIG_PATH"
+  aws s3 cp ${S3_CONFIG_PATH} ./ --recursive
+fi
 
 exec npm start
